1d1c928987b3568757fc5c36fc3138617a10f970
Jorge Freitas <80436467+jorgelcff@users.noreply.github.com>
2022-10-17 08:17:57 -0300

    add item

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame as pg
 from hero import *
-
 pg.init()
 
 largura, altura = 1080, 720
@@ -9,13 +8,14 @@ pg.display.set_caption('Dragão de Nome Impronunciável')
 background = pg.image.load("./background.jpg")
 #Aqui criamos a tela
 
-
+relogio = pg.time.Clock()
 hero = Hero(110,110, largura, altura, tela)
 bala = pg.sprite.Group()
 tempo = pg.time.get_ticks()
 
+x = largura/2 - 20
+y = 0
 while True:
-
   #Aqui checamos se o usuario clicou no X para fechar o jogo
   for event in pg.event.get():
     if event.type == pg.QUIT:
@@ -24,8 +24,17 @@ while True:
 
   hero.draw()
 
+  pg.draw.rect(tela, (255, 255, 0), (x, y, 40, 50))
+  if y >= altura:
+    y=0
+  y = y + 1
+
+
+
   pg.display.update()
   hero.update(bala, tempo)
 
   tela.blit(background, (0, 0))
+
+
 
